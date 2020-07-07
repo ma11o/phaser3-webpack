237212bf5670296b4e147be5a318d6b36dc59405
loksam <loksam.jp@gmail.com>
2020-07-07 22:10:47 +0900

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -47,10 +47,14 @@ module.exports = {
     new webpack.DefinePlugin({
       'typeof CANVAS_RENDERER': JSON.stringify(true),
       'typeof WEBGL_RENDERER': JSON.stringify(true)
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'production-dependencies',
-      filename: 'production-dependencies.bundle.js'
-    }),
-  ]
+    })
+  ],
+
+  optimization: {
+    namedChunks: true,
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
+    }
+  }
 };
